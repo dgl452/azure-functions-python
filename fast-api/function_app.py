b3575ebc0bf42c7f7677d196a0ec15f5a580a99f
Daniel González <daniel.gonzalez@global.ntt>
2022-12-07 11:37:35 +0100

    Added fast api

--- fast-api/function_app.py
+++ fast-api/function_app.py
@@ -1,8 +1,26 @@
+from fastapi import FastAPI, Request
+import nest_asyncio
 import azure.functions as func
+from azure.functions import AsgiMiddleware
+
+nest_asyncio.apply()
+
+api = FastAPI()
+
+
+@api.get("/say-hello")
+async def index(request: Request):
+    return {
+        "info": "Hello Daniel! How Are you?",
+    }
+
+
+ASGI_MIDDLEWARE = AsgiMiddleware(api)
 
 app = func.FunctionApp()
 
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello") # HTTP Trigger
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("HttpTrigger1 function processed a request!!!")
+
+@app.function_name(name="main_trigger")
+@app.route(route="{*path_info}")
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return ASGI_MIDDLEWARE.handle(req, context)
