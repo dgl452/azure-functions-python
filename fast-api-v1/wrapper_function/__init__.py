1ec31158c2f50c278e4d5a064ba2826af57cf883
Daniel González <daniel.gonzalez@global.ntt>
2022-12-07 13:39:09 +0100

    Added ASYNC with fast api

--- fast-api-v1/wrapper_function/__init__.py
+++ fast-api-v1/wrapper_function/__init__.py
@@ -1,22 +1,46 @@
-from fastapi import FastAPI, Request
 import nest_asyncio
+import base64
 import azure.functions as func
+import requests
+from fastapi import FastAPI, Request
+from typing import Union
 from azure.functions import AsgiMiddleware
+import azure.durable_functions as df
 
 nest_asyncio.apply()
 
 api = FastAPI()
 
 
-@api.get("/say-hello")
-async def index(request: Request):
+@api.get("/export")
+async def index(request: Request, starter: Union[str, None] = None):
+    starter_bytes_enc = starter.encode("ascii")
+    starter_bytes = base64.b64decode(starter_bytes_enc)
+    starter_decoded = starter_bytes.decode("ascii")
+    client = df.DurableOrchestrationClient(starter_decoded)
+    instance_id = await client.start_new("orchestrator", None, None)
     return {
-        "info": "Hello Daniel! How Are you?",
+        "expoerter_id": instance_id,
     }
+
+
+@api.get("/export/{id}/status")
+async def index(request: Request, id: str):
+    status = requests.get(
+        f"http://localhost:7071/runtime/webhooks/durabletask/instances/{id}"
+    )
+
+    return status.json()
 
 
 ASGI_MIDDLEWARE = AsgiMiddleware(api)
 
 
-async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+async def main(
+    req: func.HttpRequest, context: func.Context, starter: str
+) -> func.HttpResponse:
+    starter_bytes = starter.encode("ascii")
+    starter_base64_bytes = base64.b64encode(starter_bytes)
+    base64_starter = starter_base64_bytes.decode("ascii")
+    req._HttpRequest__url = f"{req._HttpRequest__url}?starter={ base64_starter }"
     return ASGI_MIDDLEWARE.handle(req, context)
